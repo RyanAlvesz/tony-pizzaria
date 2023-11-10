d723a6e93a8a4de4c28de03e31da727d7cd6aa56
RyanAlvesz <ra546038@gmail.com>
2023-11-10 01:38:38 -0300

    Ajustes na responsividade e código

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,324 @@
 'use strict'
 
+const bebidas = document.getElementById('bebidas')
+
+const categorias = [
+    
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Logo.svg'
+    },
+    {
+        nome: 'Soda',
+        imagem: '../img/icons/Soda.svg'
+    },
+    {
+        nome: 'Watermelon',
+        imagem: '../img/icons/Watermelon.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Pizza.svg'
+    },
+    {
+        nome: 'Sundae',
+        imagem: '../img/icons/Sundae.svg'
+    },
+    {
+        nome: 'Ice-Cream',
+        imagem: '../img/icons/Ice-Cream.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Logo.svg'
+    },
+    {
+        nome: 'Soda',
+        imagem: '../img/icons/Soda.svg'
+    },
+    {
+        nome: 'Watermelon',
+        imagem: '../img/icons/Watermelon.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Pizza.svg'
+    },
+    {
+        nome: 'Sundae',
+        imagem: '../img/icons/Sundae.svg'
+    },
+    {
+        nome: 'Ice-Cream',
+        imagem: '../img/icons/Ice-Cream.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Logo.svg'
+    },
+    {
+        nome: 'Soda',
+        imagem: '../img/icons/Soda.svg'
+    },
+    {
+        nome: 'Watermelon',
+        imagem: '../img/icons/Watermelon.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Pizza.svg'
+    },
+    {
+        nome: 'Sundae',
+        imagem: '../img/icons/Sundae.svg'
+    },
+    {
+        nome: 'Ice-Cream',
+        imagem: '../img/icons/Ice-Cream.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Logo.svg'
+    },
+    {
+        nome: 'Soda',
+        imagem: '../img/icons/Soda.svg'
+    },
+    {
+        nome: 'Watermelon',
+        imagem: '../img/icons/Watermelon.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Pizza.svg'
+    },
+    {
+        nome: 'Sundae',
+        imagem: '../img/icons/Sundae.svg'
+    },
+    {
+        nome: 'Ice-Cream',
+        imagem: '../img/icons/Ice-Cream.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Logo.svg'
+    },
+    {
+        nome: 'Soda',
+        imagem: '../img/icons/Soda.svg'
+    },
+    {
+        nome: 'Watermelon',
+        imagem: '../img/icons/Watermelon.svg'
+    },
+    {
+        nome: 'Pizza',
+        imagem: '../img/icons/Pizza.svg'
+    },
+    {
+        nome: 'Sundae',
+        imagem: '../img/icons/Sundae.svg'
+    },
+    {
+        nome: 'Ice-Cream',
+        imagem: '../img/icons/Ice-Cream.svg'
+    }
+
+]
+
+const pizzasFavoritas = [
+
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '..//img/pizza-1.webp'
+    },
+    {
+        nome: 'Pizza de peperoni com queijo',
+        preco: '19,00',
+        imagem: '../img/pizza-2.webp'
+    },
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '../img/pizza-3.webp'
+    },
+    {
+        nome: 'Pizza de peperoni com queijo',
+        preco: '19,00',
+        imagem: '../img/pizza-2.webp'
+    },
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '../img/pizza-3.webp'
+    },
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '../img/pizza-1.webp'
+    },
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '../img/pizza-1.webp'
+    },
+    {
+        nome: 'Pizza de peperoni com queijo',
+        preco: '19,00',
+        imagem: '../img/pizza-2.webp'
+    },
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '../img/pizza-3.webp'
+    },
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '../img/pizza-3.webp'
+    },
+    {
+        nome: 'Pizza de calabreza com queijo',
+        preco: '16,00',
+        imagem: '../img/pizza-1.webp'
+    }
+
+]
+
+const pizzasRecomendadas = [
+
+    {
+        nome: 'Pizza de peperoni com queijo e tomate',
+        preco: '19,65',
+        imagem: '..//img//pizza-4.webp'
+    },
+    {
+        nome: 'Pizza brotinho com queijo e tomate',
+        preco: '24,99',
+        imagem: '..//img//pizza-5.webp'
+    },
+    {
+        nome: 'Pizza de peperoni com queijo e tomate',
+        preco: '19,65',
+        imagem: '..//img//pizza-4.webp'
+    },
+    {
+        nome: 'Pizza brotinho com queijo e tomate',
+        preco: '24,99',
+        imagem: '..//img//pizza-5.webp'
+    },
+    {
+        nome: 'Pizza de peperoni com queijo e tomate',
+        preco: '19,65',
+        imagem: '..//img//pizza-4.webp'
+    },
+    {
+        nome: 'Pizza brotinho com queijo e tomate',
+        preco: '24,99',
+        imagem: '..//img//pizza-5.webp'
+    },
+    {
+        nome: 'Pizza de peperoni com queijo e tomate',
+        preco: '19,65',
+        imagem: '..//img//pizza-4.webp'
+    },
+    {
+        nome: 'Pizza brotinho com queijo e tomate',
+        preco: '24,99',
+        imagem: '..//img//pizza-5.webp'
+    },
+    {
+        nome: 'Pizza de peperoni com queijo e tomate',
+        preco: '19,65',
+        imagem: '..//img//pizza-4.webp'
+    },
+    {
+        nome: 'Pizza brotinho com queijo e tomate',
+        preco: '24,99',
+        imagem: '..//img//pizza-5.webp'
+    }
+
+]
+
+const bebibasCompra = [
+
+    {
+        nome:'Coca-Cola',
+        imagem: '../img/coca.webp',
+        preco: '14,99',
+        descricao: [
+            'Refrigerante Garrafa 2L',
+            'Embalagem Econômica' 
+        ]
+    },
+    {
+        nome:'Cerveja Heineken',
+        imagem: '../img/cerveja.webp',
+        preco: '19,99',
+        descricao: [
+            'Cerveja Heineken 500ml',
+            'Embalagem Econômica' 
+        ]
+    },
+    {
+        nome:'Coca-Cola',
+        imagem: '../img/coca.webp',
+        preco: '14,99',
+        descricao: [
+            'Refrigerante Garrafa 2L',
+            'Embalagem Econômica' 
+        ]
+    },
+    {
+        nome:'Cerveja Heineken',
+        imagem: '../img/cerveja.webp',
+        preco: '19,99',
+        descricao: [
+            'Cerveja Heineken 500ml',
+            'Embalagem Econômica' 
+        ]
+    },
+    {
+        nome:'Coca-Cola',
+        imagem: '../img/coca.webp',
+        preco: '14,99',
+        descricao: [
+            'Refrigerante Garrafa 2L',
+            'Embalagem Econômica' 
+        ]
+    },
+    {
+        nome:'Cerveja Heineken',
+        imagem: '../img/cerveja.webp',
+        preco: '19,99',
+        descricao: [
+            'Cerveja Heineken 500ml',
+            'Embalagem Econômica' 
+        ]
+    },
+    {
+        nome:'Coca-Cola',
+        imagem: '../img/coca.webp',
+        preco: '14,99',
+        descricao: [
+            'Refrigerante Garrafa 2L',
+            'Embalagem Econômica' 
+        ]
+    },
+    {
+        nome:'Cerveja Heineken',
+        imagem: '../img/cerveja.webp',
+        preco: '19,99',
+        descricao: [
+            'Cerveja Heineken 500ml',
+            'Embalagem Econômica' 
+        ]
+    }
+
+]
+
 const selecionarPizza = () => {
     
     const pizzas = document.getElementsByClassName('pizza-favorita')
@@ -8,7 +327,8 @@ const selecionarPizza = () => {
         
         pizza.addEventListener('click', () => {
 
-            localStorage.setItem('imagemPizza', pizza.classList[1])
+            console.log(pizza.style.cssText.split('"')[1])
+            localStorage.setItem('imagemPizza', pizza.style.cssText.split('"')[1])
             localStorage.setItem('nomePizza', pizza.children[0].children[1].textContent)
             localStorage.setItem('precoPizza', pizza.children[0].children[0].textContent)
 
@@ -39,13 +359,134 @@ const montarUsuario = () => {
 
 }
 
-const bebidas = document.getElementById('bebidas')
-
 bebidas.addEventListener('click', () => {
 
         window.location.href = '#bebidas'
 
 })
 
+const criarCategorias = (categoria) => {
+
+    const li = document.createElement('li')
+    const button = document.createElement('button')
+    button.classList.add('categoria')
+    const img = document.createElement('img')
+    img.src = categoria.imagem
+    img.alt = categoria.nome
+    
+    li.appendChild(button)
+    button.appendChild(img)
+
+    const container = document.getElementById('container-categorias')
+    container.appendChild(li)
+
+}
+
+const criarPizzasFavoritas = (pizza) => {
+
+    const button = document.createElement('button')
+    button.classList.add('pizza-favorita')
+    button.style.backgroundImage = `url(${pizza.imagem})`
+
+    const a = document.createElement('a')
+    a.href = './pizza.html'
+
+    const span = document.createElement('span')
+    span.textContent = `R$ ${pizza.preco}`
+    span.classList.add('preco')
+
+    const p = document.createElement('p')
+    p.textContent = pizza.nome
+    p.classList.add('nome-pizza')
+
+    button.appendChild(a)
+    a.replaceChildren(span, p)
+
+    const pizzaContainer = document.getElementById('pizzas-favoritas')
+    pizzaContainer.appendChild(button)
+
+}
+
+const criarPizzasRecomendadas = (pizza) => {
+
+    const button = document.createElement('button')
+    button.classList.add('pizza-favorita')
+    button.style.backgroundImage = `url(${pizza.imagem})`
+
+    const a = document.createElement('a')
+    a.href = './pizza.html'
+
+    const span = document.createElement('span')
+    span.textContent = `R$ ${pizza.preco}`
+    span.classList.add('preco')
+    span.style.display = 'none'
+
+    const p = document.createElement('p')
+    p.textContent = pizza.nome
+    p.classList.add('nome-pizza')
+    p.classList.add('nome-pizza-recomendada')
+
+    button.appendChild(a)
+    a.replaceChildren(span, p)
+
+    const pizzaContainer = document.getElementById('pizzas-recomendadas')
+    pizzaContainer.appendChild(button)
+    
+}
+
+const criarBebidas = (bebida) => {
+
+    const button = document.createElement('button')
+    const a = document.createElement('a')
+    const produto = document.createElement('div')
+    const img = document.createElement('img')
+    const div = document.createElement('div')
+    const h3 = document.createElement('h3')
+    const ul = document.createElement('ul')
+    const span = document.createElement('span')
+    
+    button.classList.add('bebida')
+    a.href = '#'
+    produto.classList.add('produto')
+    img.src = bebida.imagem
+    img.alt = bebida.nome
+    div.classList.add('bebida-info')
+    h3.classList.add('nome-bebida')
+    h3.textContent = bebida.nome
+    ul.classList.add('descricao-bebida')
+    span.classList.add('preco')
+    span.textContent = `R$ ${bebida.preco}`
+
+    bebida.descricao.forEach(descricao => {
+        
+        const li = document.createElement('li')
+        li.textContent = descricao
+        ul.appendChild(li)
+
+    })
+
+    button.appendChild(a)
+    a.replaceChildren(produto, span)
+    produto.replaceChildren(img, div)
+    div.replaceChildren(h3, ul)
+
+    const bebidaContainer = document.getElementById('container-bebidas')
+    bebidaContainer.appendChild(button)
+    
+}
+
 window.addEventListener('load', montarUsuario)
 window.addEventListener('load', selecionarPizza)
+
+window.addEventListener('load', categorias.forEach(categoria => {
+    criarCategorias(categoria)
+}))
+window.addEventListener('load', pizzasFavoritas.forEach(pizza => {
+    criarPizzasFavoritas(pizza)
+}))
+window.addEventListener('load', pizzasRecomendadas.forEach(pizza => {
+    criarPizzasRecomendadas(pizza)
+}))
+window.addEventListener('load', bebibasCompra.forEach(bebida => {
+    criarBebidas(bebida)
+}))
--- assets/js/pizza.js
+++ assets/js/pizza.js
@@ -5,11 +5,39 @@ const nomePizza = localStorage.getItem('nomePizza')
 const precoPizza = localStorage.getItem('precoPizza')
 const like = document.getElementById('like')
 
+const comentarios = [
+
+    {
+        nome: 'Gabriela Fernandes',
+        foto: '../img/gabriela.webp',
+        data: '08/08/2023',
+        estrelas: 3,
+        avaliacao: 'Pizza muito boa!',
+        comentario: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+    },
+    {
+        nome: 'Ryan Alves',
+        foto: '../img/ryan.webp',
+        data: '04/08/2023',
+        estrelas: 5,
+        avaliacao: 'Essa pizza é demais, recomendo.',
+        comentario: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+    },
+    {
+        nome: 'Celso Furtado',
+        foto: '../img/celso.webp',
+        data: '13/07/2023',
+        estrelas: 1,
+        avaliacao: 'Pizza chegou fria!',
+        comentario: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+    }
+
+]
+
 const criarImagem = () => {
     
-    
     const header = document.getElementById('header')
-    header.style.backgroundImage = `url(../img/${imagemPizza}.webp)`
+    header.style.backgroundImage = `url(${imagemPizza})`
 
     document.getElementsByTagName('title')[0].innerText = nomePizza
 
@@ -32,5 +60,61 @@ const darLike = () => {
 
 }
 
+const gerarComentarios = (comentario) => {
+
+    const containerComentario = document.createElement('div')
+    const containerSuperior = document.createElement('div')
+    const avaliacao = document.createElement('h3')
+    const textoComentario = document.createElement('p')
+
+    const pessoa = document.createElement('div')
+    const estrelas = document.createElement('div')
+
+    const img = document.createElement('img')
+    const infoPessoa = document.createElement('div')
+
+    const nomePessoa = document.createElement('h3')
+    const data = document.createElement('span')
+
+    let qntEstrelas = comentario.estrelas
+
+    while(qntEstrelas){
+        const estrela = document.createElement('img')
+        estrela.src = '../img/icons/Star.svg'
+        estrela.alt = 'Estrela'
+        estrelas.appendChild(estrela)
+        qntEstrelas--
+    }
+    
+    containerComentario.classList.add('container-comentario')
+    containerSuperior.classList.add('container-superior')
+    avaliacao.classList.add('avaliacao')
+    avaliacao.textContent = comentario.avaliacao
+    textoComentario.classList.add('comentario')
+    textoComentario.textContent = comentario.comentario
+    pessoa.classList.add('pessoa')
+    estrelas.classList.add('estrelas')
+    img.src = comentario.foto
+    img.alt = comentario.nome
+    infoPessoa.classList.add('info-pessoa')
+    nomePessoa.classList.add('nome-pessoa')
+    nomePessoa.textContent = comentario.nome
+    data.classList.add('data')
+    data.textContent = comentario.data
+
+    containerComentario.replaceChildren(containerSuperior, avaliacao, textoComentario)
+    containerSuperior.replaceChildren(pessoa, estrelas)
+    pessoa.replaceChildren(img, infoPessoa)
+    infoPessoa.replaceChildren(nomePessoa, data)
+
+
+    const containerComentarios = document.getElementById('container-comentarios')
+    containerComentarios.appendChild(containerComentario)
+
+}
+
 like.addEventListener('click', darLike)
 window.addEventListener('load', criarImagem)
+window.addEventListener('load', comentarios.forEach(comentario => {
+    gerarComentarios(comentario)
+}))
