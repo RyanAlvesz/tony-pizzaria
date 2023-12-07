1004106c235bcc2b31a9e2d6dae457a82374124b
RyanAlvesz <ra546038@gmail.com>
2023-12-06 23:07:20 -0300

    atualizando integração com back-end

--- assets/js/funcoes-api.js
+++ assets/js/funcoes-api.js
@@ -2,7 +2,7 @@
 
 const listarUsuarios = async() => {
 
-    const url = 'http://localhost:8080/usuarios/'
+    const url = 'https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/usuarios/'
     const response = await fetch(url)
     const data = response.json()
     return data
@@ -11,7 +11,7 @@ const listarUsuarios = async() => {
 
 const listarCategorias = async() => {
 
-    const url = 'http://localhost:8080/categorias/'
+    const url = 'https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/categorias/'
     const response = await fetch(url)
     const data = response.json()
     return data
@@ -20,7 +20,7 @@ const listarCategorias = async() => {
 
 const listarPizzas = async() => {
 
-    const url = 'http://localhost:8080/pizzas/'
+    const url = 'https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/pizzas/'
     const response = await fetch(url)
     const data = response.json()
     return data
@@ -29,7 +29,7 @@ const listarPizzas = async() => {
 
 const listarPizzasFavoritas = async() => {
 
-    const url = 'http://localhost:8080/pizzas/favoritas'
+    const url = 'https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/pizzas/favoritas'
     const response = await fetch(url)
     const data = response.json()
     return data
@@ -38,7 +38,7 @@ const listarPizzasFavoritas = async() => {
 
 const listarBebidas = async() => {
 
-    const url = 'http://localhost:8080/bebidas/'
+    const url = 'https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/bebidas/'
     const response = await fetch(url)
     const data = response.json()
     return data
@@ -47,7 +47,7 @@ const listarBebidas = async() => {
 
 const infoUsuario = async(id) => {
 
-    const url = `http://localhost:8080/usuario/${id}`
+    const url = `https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/usuario/${id}`
     const response = await fetch(url)
     const data = response.json()
     return data
@@ -56,7 +56,7 @@ const infoUsuario = async(id) => {
 
 const infoPizza = async(id) => {
 
-    const url = `http://localhost:8080/pizza/${id}`
+    const url = `https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/pizza/${id}`
     const response = await fetch(url)
     const data = response.json()
     return data
@@ -65,7 +65,7 @@ const infoPizza = async(id) => {
 
 const comentarios = async(id) => {
 
-    const url = `http://localhost:8080/comentarios/${id}`
+    const url = `https://back-end-tony-pizzaria-qsx6e80e6-ryan-alves-projects.vercel.app/comentarios/${id}`
     const response = await fetch(url)
     const data = response.json()
     return data
